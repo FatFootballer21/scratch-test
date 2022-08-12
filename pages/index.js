f3ae2c449520d2b9c79e4e34b9bd81507e5cebd7
FatFootballer21 <104787095+FatFootballer21@users.noreply.github.com>
2022-08-12 15:57:40 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Blockly from 'blockly';
 
 export default function Home() {
   return (
